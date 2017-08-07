e35e517ae546b8cd71ab71608b29121d1e3a55eb
soxnanogaye <nanandieng@gmail.com>
2017-08-07 12:41:24 +0000

    calculet la longueu rdune cchaine de caractere

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char chaine[100] ;
+    int longueurChaine = 0;
+
+      printf("saisir votre variable\n");
+      scanf("%s",&chaine);
+    // On récupère la longueur de la chaîne dans longueurChaine
+      longueurChaine = strlen(chaine);
+
+    // On affiche la longueur de la chaîne
+      printf("La chaine %s fait %d caracteres de long", chaine, longueurChaine);
+
+    return 0;
+}
